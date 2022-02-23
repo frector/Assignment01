a318c294552b1bcc2963f76cf79c21198a7e9e15
frector <ripped99@hotmail.com>
2022-02-22 21:54:47 -0800

    completed assignment

--- js/script.js
+++ js/script.js
@@ -1,1 +1,111 @@
-//comment
+//STEP 1    let someMonth;
+//          function theMonth();
+//          let currentMonth;
+//          let summerMonth;
+//          let myLibraryFunction
+
+//STEP 2    3.14159
+//          'This is a string'
+//          True
+//          null
+
+//STEP 3    let gigaWatt= 0.605 * 2;
+//          let timeMachine = 'DeLorean ' + 'flux capacitor';
+
+//STEP 4    let firstName;
+//          let lastName;
+//          let address;
+//          let city;
+//          let state;
+//          let zipCode;
+//          let yourAge;
+//          let referralSource;
+//          let mayWeContactYou;
+
+//STEP 5    let firstName = 'Hector'; 
+//          let zipCode = 91915;
+//          let state = 'CA';
+
+//STEP 6    let timeTravel = 88 + ' miles per hour';
+//          console.log(timeTravel);
+
+//STEP 7    let flyingDMC = 'Roads Needed ' + false;
+//          let currentYear = 1985 + true;
+//          console.log(flyingDMC);
+//          console.log(currentYear);
+
+//STEP 8    Not Valid- Here's how to fix it:
+//          let someString = 'Who once said, \"Only two things are infinite, the universe \
+//          and human stupidity, and I\'m not sure about the former.\"';
+//          console.log(someString);
+
+//STEP 9    let x = null;
+//          console.log(x);
+//          let y;
+//          console.log(y);
+
+
+//STEP 10   console.log(typeof 88);
+//          console.log(typeof true);
+//          console.log(typeof 'DeLorean');
+//          console.log(typeof null);
+//          console.log(typeof x);
+
+//STEP 11   window.alert('Hello ' + 'Hector Romero' + ', welcome to the JavaScript class!');
+
+//STEP 12   let name = 'Hector Romero';
+//          window.alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+//STEP 13   let course = 'JavaScript';
+//          let name = 'Hector Romero';
+//          window.alert('Hello ' + name + ', welcome to the ' + course + ' class!');
+
+//STEP 14   let course = 'JavaScript';
+//          let name = 'Hector Romero';
+//          window.alert('Hello ' + name + '. \nWelcome to the ' + course + ' class!');
+
+//STEP 15   let course = 'JavaScript';
+//          let name = window.prompt('What is your name?');
+//          window.alert('Hello ' + name + '. \nWelcome to the ' + course + ' class!');
+
+//STEP 16   let course = window.prompt('What class are you taking?');
+//          let name = window.prompt('What is your name?');
+//          window.alert('Hello ' + name + '. \nWelcome to the ' + course + ' class!');
+
+//STEP 17   let x = 10;
+//          let y = 20;
+//          console.log(x + y);
+
+//STEP 18   let x = 20;
+//          console.log(x + 20);
+
+//STEP 19   let x = 20;
+//          x = x * 5;
+//          console.log(x);
+
+//STEP 20   let x = 20 % 3;
+//          x = x / 1;
+//          console.log(x);
+
+
+//STEP 21   function isItTrue(z) {
+//              if ( z == 10 && z > 8) {
+//          console.log('Yup');}}
+//          isItTrue(10);
+
+//STEP 22   function isItFalse(x) {
+//          if ( x < 12 || x != 13) {
+//              console.log('It\'s True');}
+//          else {
+//              console.log('It\'s False');}}
+//         isItFalse(13);
+
+
+
+
+
+
+
+
+
+
